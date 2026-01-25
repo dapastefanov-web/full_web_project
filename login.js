8e4efa5c5339a5687fbfcfa38dbe68a10f5c2c43
Daniel <da.pa.stefanov@gmail.com>
2026-01-26 00:17:03 +0200

    added comments and made code more clear

--- login.js
+++ login.js
@@ -1,39 +1,45 @@
 function register(){
+    //save personal info to constats when "register" button is clicked
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const phone_number = document.getElementById("phone_number").value;
-    if (name === ""){
-        alert("type in your name");
-    }
-    else if (email === ""){
-        alert("type in your Email");
-    }
-    else if (password === ""){
-        alert("type in a Password");
-    }
-    else{
-    alert("You are registered "+ name);
-    document.title = "Starting page";
-    document.getElementById("registration_page").style.display = "none";
-    recepti();  
+    //check if the person has entered all the info that is needed
+    switch(true){
+        case name == null:
+            alert("type in your name");
+            break;
+        case email == null:
+            alert("type in your Email");
+            break;
+        case password == null:
+            alert("type in a Password");
+            break;
+        default:
+            alert("You are registered "+ name);
+            document.title = "Starting page";
+            //remove the registration page
+            document.getElementById("registration_page").style.display = "none";
+            recepti();
     }
 }
 async function recepti(){
+    //fetch recipes from json file
     const res = await fetch('/api.json');
     const json = await res.json();
-    const recepti = json.recepti;
+    const recipes = json.recepti;
     let html = "";
-    recepti.forEach(function(recepta){
+    //put every recipe title with it's ingrediants in an html variable as list elements
+    recipes.forEach(function(recipe){
         html += `<li>
-        <h1> ${recepta.title} </h1>
+        <h1> ${recipe.title} </h1>
         <h3>sastavki:</h3>
         <ul>`;
-        recepta.sastavki.forEach(function(sastavka){
-            html += `<li> ${sastavka} </li>`
+        recipe.ingredients.forEach(function(ingredient){
+            html += `<li> ${ingredient} </li>`
         });
         html += '</ul></li>'
     });
-    document.getElementById("registration_page").style.display = "none";
-    document.getElementById("recepti").innerHTML = `<ul>${html}</ul>`;
+    //puting the html variable in to the HTML as an unsorted list
+    document.getElementById("recipes").innerHTML = `<ul>${html}</ul>`;
 }
